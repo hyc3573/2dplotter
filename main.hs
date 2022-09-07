22251af49c31317676ccaacc5895d5c13eb90fe2
hyc3573 <hyc3573@github.com>
2022-09-07 23:10:33 +0900

    축 엔드캡

--- main.hs
+++ main.hs
@@ -6,8 +6,10 @@ wallThickness = 2
 frontWallThickness = 10
 axisGap = 30
 axisDiameter = 8
-beltWidth = 8
-beltHeight = 10
+beltWidth = 7
+beltHeight = 17
+sideWalGap = 35
+pullyHoleDiameter = 5
 
   
 frontWall = rect3 (V3 0 0 0) (V3
@@ -16,7 +18,7 @@ frontWall = rect3 (V3 0 0 0) (V3
                           (frontWallThickness))
 
 frontHole = let hole = cylinder (axisDiameter/2) 20
-                beltHole = cube True (V3 beltWidth beltHeight 10) 
+                beltHole = cube True (V3 beltWidth beltHeight (frontWallThickness*2)) 
             in (V3 (stepperHeight) (stepperWidth/2+wallThickness) 0)
                `translate` union [hole,
                                   translate (V3 axisGap 0 0) hole,
@@ -35,16 +37,14 @@ sideWall = rect3 (V3
              (frontWallThickness))
 
 
-out =
-  let length = 600
-      stepperWidth = 42
-      stepperHeight = 40
-      thickness = 2
-      axisGap = 30
-      axleDia = 8
-      beltWidth = 5
-      beltHeight = 15
-      gap = 1
-  in union [front, sideWall]
+stepperSide = union [front, sideWall]
+noStepperSide = let wall1 = translate (V3 (stepperHeight-axisDiameter/2) 0 0) sideWall
+                    wall2 = translate (V3 (stepperHeight+axisGap+axisDiameter/2+wallThickness) 0 0) sideWall
+                    hole =
+                      translate (V3 0 (stepperWidth/2+wallThickness) (-stepperWidth/2-wallThickness)) .
+                      rotate3V (pi/2) (V3 0 1 0) $
+                      cylinder (pullyHoleDiameter/2) (stepperHeight*3)
+                in union [front, union [wall1, wall2] `difference` [hole]]
   
-main = writeSTL 2 "test.stl" out
+main = do writeSTL 2 "stepperside.stl" stepperSide
+          writeSTL 2 "nostepperside.stl" noStepperSide
