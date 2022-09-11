1be54bad020d4178ab7e23e925bd1cd91c279922
hyc3573 <hyc3573@gmail.com>
2022-09-11 21:16:40 +0900

    adding screw hole

--- main.hs
+++ main.hs
@@ -9,7 +9,9 @@ axisDiameter = 8
 beltWidth = 7
 beltHeight = 17
 sideWalGap = 35
+pullyHoleDiameter :: Double
 pullyHoleDiameter = 5
+mountHoldDiameter = 3
 
   
 frontWall = rect3 (V3 0 0 0) (V3
@@ -18,32 +20,59 @@ frontWall = rect3 (V3 0 0 0) (V3
                           (frontWallThickness))
 
 frontHole = let hole = cylinder (axisDiameter/2) 20
-                beltHole = cube True (V3 beltWidth beltHeight (frontWallThickness*2)) 
-            in (V3 (stepperHeight) (stepperWidth/2+wallThickness) 0)
-               `translate` union [hole,
-                                  translate (V3 axisGap 0 0) hole,
-                                  translate (V3 (axisGap/2) 0 0)
-                                  beltHole]
+                beltHole =
+                  cube True (V3
+                             beltWidth
+                             beltHeight
+                             (frontWallThickness*2)) 
+
+            in V3 stepperHeight (stepperWidth/2+wallThickness) 0
+               `translate`
+               union [ hole
+                     , translate (V3 axisGap 0 0) hole
+                     , translate (V3 (axisGap/2) 0 0) beltHole]
 
 front = difference frontWall [frontHole]
 
-sideWall = rect3 (V3
-                  (-wallThickness)
-                  0
-                  (-stepperWidth-wallThickness))
-           (V3
-             (0)
-             (stepperWidth+wallThickness*2)
-             (frontWallThickness))
+sideWall = V3
+           (-wallThickness)
+           0
+           (-stepperWidth-wallThickness)
+           `rect3`
+           V3
+           0
+           (stepperWidth+wallThickness*2)
+           frontWallThickness
 
+stepperMountHole = let hole = translate (V3 (-10) 0 0) $ rotate3V (pi/2) (V3 0 1 0) $ cylinder (mountHoldDiameter/2) 150
+                   in union $ fmap (flip translate $ hole) [ V3 0 0 0
+                                                           , V3 0 10 10]
 
-stepperSide = union [front, sideWall]
-noStepperSide = let wall1 = translate (V3 (stepperHeight-axisDiameter/2) 0 0) sideWall
-                    wall2 = translate (V3 (stepperHeight+axisGap+axisDiameter/2+wallThickness) 0 0) sideWall
+stepperSide :: SymbolicObj3
+stepperSide = let sideWall2 =
+                    V3 (stepperHeight*2+axisGap) 0 0
+                    `translate`
+                    sideWall
+              in union [ front
+                       , sideWall
+                       , sideWall2
+                       , stepperMountHole] `difference` []
+noStepperSide = let wall1 =
+                      V3 (stepperHeight-axisDiameter/2) 0 0
+                      `translate`
+                      sideWall
+
+                    wall2 =
+                      V3 (stepperHeight+axisGap+axisDiameter/2+wallThickness) 0 0
+                      `translate`
+                      sideWall
+
                     hole =
                       translate (V3 0 (stepperWidth/2+wallThickness) (-stepperWidth/2-wallThickness)) .
                       rotate3V (pi/2) (V3 0 1 0) $
-                      cylinder (pullyHoleDiameter/2) (stepperHeight*3)
+
+                      cylinder
+                      (pullyHoleDiameter/2) (stepperHeight*3)
                 in union [front, union [wall1, wall2] `difference` [hole]]
   
 main = do writeSTL 2 "stepperside.stl" stepperSide
